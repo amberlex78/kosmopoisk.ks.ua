ec036b6d4c8c4d34770b076b189e0839015beb86
Alexey Abrosimov <amberlex78@email.com>
2021-06-24 10:08:39 +0300

    description a changes

--- application/views/backend/dashboard/v_index.php
+++ application/views/backend/dashboard/v_index.php
@@ -2,20 +2,20 @@
 
 <h4 class="text-success">Привет, <?php echo $user->username ?> :)</h4>
 <hr>
-<h5>Обновления CMS:</h5>
+<h5>Обновления на сайте «Херсон – Космопоиск»</h5>
 <p><span class="label label-info">[2021.06.24]</span> В админке для добавления или редактирования статьи поля: "Теги" и "Первоисточник" теперь заполняются под "Полный текст статьи". Вкладка "Другое", где они раньше находились - убрана, т.к. в этом случае нужно совершать меньше движений при заполнении, ну и не забывать их заполнять, если требуется</p>
 <p><?php echo HTML::image('uploads/images/tags-link.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
 <p><span class="label label-info">[2021.06.24]</span> Если залогинены под админом - можно редактировать статью прямо с сайта нажав на значок <i class="icon-edit"></i> который будет виден перед названием статьи</p>
 <p><?php echo HTML::image('uploads/images/edit_article.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
-<p><span class="label label-info">[2021.06.24]</span> Внизу на главной показывается <a href="https://kosmopoisk.ks.ua#tags" target="_blank">список тегов</a></p>
+<p><span class="label label-info">[2021.06.24]</span> Внизу сайта на главной показывается <a href="https://kosmopoisk.ks.ua#tags" target="_blank">список тегов</a></p>
 <p><?php echo HTML::image('uploads/images/tags.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
-<p><span class="label label-info">[2021.06.22]</span> Если залогинены, в меню будет кнопка '+ Добавить статью' для удобного пользования и меньших телодвижений.<br>Так же кнопка 'В админку'</p>
+<p><span class="label label-info">[2021.06.22]</span> Если залогинены, на сайте в меню будет кнопка '+ Добавить статью' для удобного пользования и меньших телодвижений.<br>Так же кнопка 'В админку'</p>
 <p><?php echo HTML::image('uploads/images/add_article.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
-<p><span class="label label-info">[2021.06.02]</span> Перенес панель с меню вправо. Теперь при добавлении или редактировании статьи редактор текста получается почти посредине экрана, что удобнее.</p>
+<p><span class="label label-info">[2021.06.02]</span> В админке перенес панель с меню вправо. Теперь при добавлении или редактировании статьи редактор текста получается почти посредине экрана, что удобнее.</p>
 <p><?php echo HTML::image('uploads/images/panel.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
 <p><span class="label label-info">[2012.10.30]</span> При добавлении (редактировании) статьи появилась вкладка "Другое", туда перенесены теги статьи и добавлено поле для ссылки на первоисточник статьи, если такавой есть</p>
