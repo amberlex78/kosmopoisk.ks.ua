748a26bfb5eb9c125df140296ba4dd1ad42d739a
Alexey Abrosimov <amberlex78@email.com>
2021-06-24 10:14:47 +0300

    description a changes

--- application/views/backend/dashboard/v_index.php
+++ application/views/backend/dashboard/v_index.php
@@ -1,18 +1,21 @@
 <?php defined('SYSPATH') or die('No direct script access.') ?>
 
 <h4 class="text-success">Привет, <?php echo $user->username ?> :)</h4>
+
+<span class="label label-info label-large"><?php echo 'Последний раз авторизовались: ' . date('d.m.Y - H:i', $user->last_login) ?></span>
+
 <hr>
 <h5>Обновления на сайте «Херсон – Космопоиск»</h5>
 <p><span class="label label-info">[2021.06.24]</span> В админке для добавления или редактирования статьи поля: "Теги" и "Первоисточник" теперь заполняются под "Полный текст статьи". Вкладка "Другое", где они раньше находились - убрана, т.к. в этом случае нужно совершать меньше движений при заполнении, ну и не забывать их заполнять, если требуется</p>
 <p><?php echo HTML::image('uploads/images/tags-link.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
-<p><span class="label label-info">[2021.06.24]</span> Если залогинены под админом - можно редактировать статью прямо с сайта нажав на значок <i class="icon-edit"></i> который будет виден перед названием статьи</p>
+<p><span class="label label-info">[2021.06.24]</span> Если авторизованы - можно редактировать статью прямо с сайта нажав на значок <i class="icon-edit"></i> который будет виден перед названием статьи</p>
 <p><?php echo HTML::image('uploads/images/edit_article.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
 <p><span class="label label-info">[2021.06.24]</span> Внизу сайта на главной показывается <a href="https://kosmopoisk.ks.ua#tags" target="_blank">список тегов</a></p>
 <p><?php echo HTML::image('uploads/images/tags.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
-<p><span class="label label-info">[2021.06.22]</span> Если залогинены, на сайте в меню будет кнопка '+ Добавить статью' для удобного пользования и меньших телодвижений.<br>Так же кнопка 'В админку'</p>
+<p><span class="label label-info">[2021.06.22]</span> Если авторизованы, на сайте в меню будет кнопка '+ Добавить статью' для удобного пользования и меньших телодвижений.<br>Так же кнопка 'В админку'</p>
 <p><?php echo HTML::image('uploads/images/add_article.png', array('class' => 'img-rounded', 'style' => 'width: 870px')) ?></p>
 <hr>
 <p><span class="label label-info">[2021.06.02]</span> В админке перенес панель с меню вправо. Теперь при добавлении или редактировании статьи редактор текста получается почти посредине экрана, что удобнее.</p>
@@ -49,5 +52,3 @@
 		<td><?php echo $info['phpversion'] ?></td>
 	</tr>
 </table>
-
-<span class="label label-info"><?php echo 'Последний раз авторизовались: ' . date('d.m.Y - H:i', $user->last_login) ?></span>
