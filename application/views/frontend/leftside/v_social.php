b368066909113a47b161e0a8d75a20783f908d0f
Alexey Abrosimov <amberlex78@email.com>
2021-06-24 10:42:19 +0300

    calend.ru http => https

--- application/views/frontend/leftside/v_social.php
+++ application/views/frontend/leftside/v_social.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.') ?>
 
 <div style="text-align: center">
-    <a href=http://www.calend.ru/holidays/ukraine/ target=_blank>
-        <img src="http://www.calend.ru/img/export/informer_6.png" width="250" alt="Праздники Украины">
+    <a href=https://www.calend.ru/holidays/ukraine/ target=_blank>
+        <img src="https://www.calend.ru/img/export/informer_6.png" width="250" alt="Праздники Украины">
     </a>
 </div>
